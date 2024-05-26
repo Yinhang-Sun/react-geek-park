2c96ff8a1502c0642e04502d799e802b6b15b83a
yinhang <sunyinhang@gmail.com>
2024-05-26 14:48:57 -0700

    Prepare static components

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,29 @@
+import './index.scss'
+import { Card, Form, Input, Button } from 'antd'
+import logo from '@/assets/logo.png'
+
 const Login = () => {
-    return <div>this is Login</div>
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* Login Form */}
+        <Form>
+          <Form.Item>
+            <Input size="large" placeholder="Please enter phone number" />
+          </Form.Item>
+          <Form.Item>
+            <Input size="large" placeholder="Please enter verification code" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
 }
 
 export default Login
