5c9c30d5011454ab986921ef5604c852502daec9
yinhang <sunyinhang@gmail.com>
2024-05-28 15:47:56 -0700

    Click menu to jump router

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,12 +1,69 @@
-// test if token is injected successfully or not 
-import { request } from "@/utils"
-import { useEffect } from "react"
+import { Layout, Menu, Popconfirm } from 'antd'
+import {
+    HomeOutlined,
+    DiffOutlined,
+    EditOutlined,
+    LogoutOutlined,
+} from '@ant-design/icons'
+import './index.scss'
+import { Outlet, useNavigate } from 'react-router-dom'
 
-const Layout = () => {
-    useEffect(() => {
-        request.get('/user/profile')
-    }, [])
-    return <div>this is Layout</div>
+const { Header, Sider } = Layout
+
+const items = [
+    {
+        label: 'Home',
+        key: '/',
+        icon: <HomeOutlined />,
+    },
+    {
+        label: 'Manage Article',
+        key: '/article',
+        icon: <DiffOutlined />,
+    },
+    {
+        label: 'Create Article',
+        key: '/publish',
+        icon: <EditOutlined />,
+    },
+]
+
+const GeekLayout = () => {
+    const navigate = useNavigate()
+    const onMenuClick = (route) => {
+        console.log("Menu is clicked!", route)
+        const path = route.key
+        navigate(path)
+    }
+    return (
+        <Layout>
+            <Header className="header">
+                <div className="logo" />
+                <div className="user-info">
+                    <span className="user-name">Teacher John</span>
+                    <span className="user-logout">
+                        <Popconfirm title="Are you sure to log out?" okText="Logout" cancelText="Cancel">
+                            <LogoutOutlined /> Logout
+                        </Popconfirm>
+                    </span>
+                </div>
+            </Header>
+            <Layout>
+                <Sider width={200} className="site-layout-background">
+                    <Menu
+                        mode="inline"
+                        theme="dark"
+                        defaultSelectedKeys={['1']}
+                        onClick={onMenuClick}
+                        items={items}
+                        style={{ height: '100%', borderRight: 0 }}></Menu>
+                </Sider>
+                <Layout className="layout-content" style={{ padding: 20 }}>
+                    {/* Outlet of secondary routing */}
+                    <Outlet />
+                </Layout>
+            </Layout>
+        </Layout>
+    )
 }
-
-export default Layout
+export default GeekLayout
--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,29 @@ import Login from "@/pages/Login";
 
 import { createBrowserRouter } from "react-router-dom";
 import { AuthRoute } from "@/components/AuthRoute";
+import Home from "@/pages/Home";
+import Article from "@/pages/Article";
+import Publish from "@/pages/Publish";
 
 // configure router instance 
 const router = createBrowserRouter([
     {
         path: "/", 
-        element: <AuthRoute><Layout /></AuthRoute>
+        element: <AuthRoute><Layout /></AuthRoute>, 
+        children: [
+            {
+                index: true,
+                element: <Home />
+            }, 
+            {
+                path: 'article', 
+                element: <Article />
+            }, 
+            {
+                path: 'publish', 
+                element: <Publish />
+            }
+        ]
     }, 
     {
         path: "/login", 
