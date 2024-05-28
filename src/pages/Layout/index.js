20f50c0fe06516722168b11b1b96258e66f5f819
yinhang <sunyinhang@gmail.com>
2024-05-28 12:44:47 -0700

    Inject token into axios headers

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,4 +1,11 @@
+// test if token is injected successfully or not 
+import { request } from "@/utils"
+import { useEffect } from "react"
+
 const Layout = () => {
+    useEffect(() => {
+        request.get('/user/profile')
+    }, [])
     return <div>this is Layout</div>
 }
 
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,35 +1,43 @@
 // axios encapsulation 
 import axios from "axios";
+import { getToken } from "./token";
 
 // 1. Root domain name configuration
 // 2. Timeout period
 // 3. Request interceptor / response interceptor
 
 const request = axios.create({
-    baseURL: 'http://geek.itheima.net/v1_0', 
-    timeout: 5000
+  baseURL: 'http://geek.itheima.net/v1_0',
+  timeout: 5000
 })
 
 //Add request interceptor
 //Before sending the request, 
 //intercept and insert some custom configuration (parameter processing)
-request.interceptors.request.use((config)=> {
-    return config
-  }, (error)=> {
-    return Promise.reject(error)
+request.interceptors.request.use((config) => {
+  // operate config to inject token 
+  // 1. get token 
+  // 2. Token splicing according to the backend format requirements
+  const token = getToken()
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+}, (error) => {
+  return Promise.reject(error)
 })
 
 //Add response interceptor
 //Before the response is returned to the client, 
 //intercept it and focus on processing the returned data.
-request.interceptors.response.use((response)=> {
-    // Status codes within the 2xx range will trigger this function.
-    //Do something with the response data
-    return response.data
-  }, (error)=> {
-    // Status codes outside the 2xx range will trigger this function.
-    // Do something with the response error
-    return Promise.reject(error)
+request.interceptors.response.use((response) => {
+  // Status codes within the 2xx range will trigger this function.
+  //Do something with the response data
+  return response.data
+}, (error) => {
+  // Status codes outside the 2xx range will trigger this function.
+  // Do something with the response error
+  return Promise.reject(error)
 })
 
 export { request }
