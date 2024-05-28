44ff49e04c4fb3481c8afdcb31fe99ef1a39d30d
yinhang <sunyinhang@gmail.com>
2024-05-28 11:32:09 -0700

    Redux manage token -login logic

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,15 +1,21 @@
 import './index.scss'
-import { Card, Form, Input, Button } from 'antd'
+import { Card, Form, Input, Button, message } from 'antd'
 import logo from '@/assets/logo.png'
 import { useDispatch } from 'react-redux'
 import { fetchLogin } from '@/store/modules/user'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
   const dispatch = useDispatch()
-  const onFinish = (values) => {
+  const navigate = useNavigate()
+  const onFinish = async (values) => {
     console.log(values);
     // Trigger asychronous action, fetchLogin 
-    dispatch(fetchLogin(values))
+    await dispatch(fetchLogin(values))
+    // 1. Jump to homepage
+    navigate('/')
+    // 2. Prompt the user
+    message.success('Login successfully!')
   }
   return (
     <div className="login">
