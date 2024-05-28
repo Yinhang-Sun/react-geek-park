db34da7494c7d613f9ec184b0808ca068524d281
yinhang <sunyinhang@gmail.com>
2024-05-28 11:16:32 -0700

    Redux manage token - token asychronous fetching

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,7 @@
 // states related to user management 
 
 import { createSlice } from "@reduxjs/toolkit";
+import { request } from "@/utils"
 
 const userStore = createSlice({
     name: "user", 
@@ -22,6 +23,18 @@ const { setToken } = userStore.actions
 // get reducer function 
 const userReducer = userStore.reducer
 
-export { setToken } 
+// Asynchronous method, complete login and obtain token
+const fetchLogin = (loginForm) => {
+    return async (dispatch) => {
+        // 1. send asynchronous request 
+        const res = await request.post('/authorizations', loginForm)
+        // 2. submit synchronous action to store token 
+        dispatch(setToken(res.data.token))
+    }
+}
+
+export { fetchLogin, setToken } 
+
+
 export default userReducer
 
