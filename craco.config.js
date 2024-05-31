e73e3f1992063dd5262a86668aca50d467e7c6fb
yinhang <sunyinhang@gmail.com>
2024-05-30 22:25:00 -0700

    Package volume analyzing by source-map-explorer

--- craco.config.js
+++ craco.config.js
@@ -3,12 +3,12 @@
 const path = require('path')
 
 module.exports = {
-   // webpack configuration
-   webpack: {
-     //Configure alias
-     alias: {
-       //Convention: Use @ to indicate the path where the src file is located
-       '@': path.resolve(__dirname, 'src')
-     }
-   }
+  // webpack configuration
+  webpack: {
+    //Configure alias
+    alias: {
+      //Convention: Use @ to indicate the path where the src file is located
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 }
